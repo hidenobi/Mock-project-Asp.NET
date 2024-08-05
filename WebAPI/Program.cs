e93860499c649b8e35de166a459332be7e7a36af
TuanVH <dungtuan095@gmail.com>
2024-08-05 16:16:32 +0700

    convert to mysql

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -20,22 +20,25 @@ var serverVersion = new MySqlServerVersion(new Version(8, 0, 23));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, serverVersion));
 
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IGovernmentOfficeRegionRepository, GovernmentOfficeRegionRepository>();
 builder.Services.AddScoped<IGovernmentOfficeRegionService, GovernmentOfficeRegionService>();
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 builder.Services.AddScoped<IProgrammeRepository, ProgrammeRepository>();
 builder.Services.AddScoped<IProgrammeService, ProgrammeService>();
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
 builder.Services.AddScoped<IManagerNameRepository, ManagerNameRepository>();
 builder.Services.AddScoped<ContactService>();
 builder.Services.AddScoped<ManagerNameService>();
 builder.Services.AddScoped<IGovernmentOfficeRegionRepository, GovernmentOfficeRegionRepository>();
 builder.Services.AddScoped<IGovernmentOfficeRegionService, GovernmentOfficeRegionService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IAddressRepository, AddressRepository>();
 builder.Services.AddScoped<IAddressService, AddressService>();
 builder.Services.AddScoped<IBusinessTypeRepository, BusinessTypeRepository>();
 builder.Services.AddScoped<IBusinessTypeService, BusinessTypeService>();
+
 
 builder.Services.AddCors(options =>
 {
@@ -52,7 +55,6 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -67,7 +69,6 @@ app.UseCors("AllowAllOrigins");
 app.UseStaticFiles();
 
 app.UseRouting();
-
 
 
 app.Run();
